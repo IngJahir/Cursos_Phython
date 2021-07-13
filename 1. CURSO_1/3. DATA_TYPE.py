188dd7a446027c8f2be5bd0fbae57f93325465be
IngJahir <jahirtautiva@gmail.com>
2021-07-12 20:22:17 -0500

    Tipos de datos en Python

--- 1. CURSO_1/3. DATA_TYPE.py
+++ 1. CURSO_1/3. DATA_TYPE.py
@@ -2,10 +2,10 @@
 num_entero = 10
 print(num_entero)
 
-num_real = 3.1416
+num_real = 3.141516
 print(num_real)
 
-tipo_cadena = "Hola mundo"
+tipo_cadena = "Hola mundo, bienvenidos a Python"
 print(tipo_cadena)
 
 tipo_bool = True
